6ad38f74d8558b47cc7e99ed3166e959486a86a6
Лапихин Александр <94218678+Al-Lapik@users.noreply.github.com>
2022-02-18 23:20:27 +0300

    B178 Lapikhin Alexander

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,37 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value == 1)
+    return false;
+  for (int i = 2; i <= sqrt(value); i++)
+    if (value % i == 0)
+      return false;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int numberPrime = 0, value = 0;
+  do {
+    value++;
+    if (checkPrime(value)) {
+      numberPrime++;
+    }
+  } while (numberPrime < n);
+  return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  value++;
+  if (checkPrime(value))
+    return value;
+  return nextPrime(value);
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  hbound--;
+  if (hbound == 1)
+    return 0;
+  if (checkPrime(hbound))
+    return hbound + sumPrime(hbound);
+  return sumPrime(hbound);
 }
